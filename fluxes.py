d927d43f764dc5ee0532eb0bd92e126447946d72
Christian Diener <ch.diener@gmail.com>
2016-09-11 19:23:59 -0500

    Linted fluxes.py.

--- fluxes.py
+++ fluxes.py
@@ -8,8 +8,7 @@
 
 from cobra.solvers import solver_dict, get_solver_name
 from cobra.manipulation.modify import convert_to_irreversible
-from cobra.io import read_sbml_model, load_matlab_model
-from cobra import Reaction
+from cobra.io import read_sbml_model
 from urllib.request import urlopen
 from math import ceil
 import zipfile
@@ -18,7 +17,9 @@ import os
 import sys
 import shutil
 
-HMRURL = "http://www.metabolicatlas.org/assets/hmr/HMRcollection.xml-8849b3803fdcc7dbd26fea46ef0dcc50.zip"
+HMRURL = ("http://www.metabolicatlas.org/assets/hmr/"
+          "HMRcollection.xml-8849b3803fdcc7dbd26fea46ef0dcc50.zip")
+
 
 def download_model(url, basename, dir="models"):
     """Downloads a model and changes its compression to gzip
@@ -43,7 +44,8 @@ def download_model(url, basename, dir="models"):
     os.makedirs(dir, exist_ok=True)
     basename = os.path.join(dir, basename.replace(" ", "_"))
 
-    if os.path.exists(basename + ".xml.gz"): return
+    if os.path.exists(basename + ".xml.gz"):
+        return
 
     with urlopen(url) as response, open(basename + ".zip", "wb") as f_out:
             shutil.copyfileobj(response, f_out)
@@ -52,10 +54,11 @@ def download_model(url, basename, dir="models"):
         xml_files = [z for z in zipf.namelist() if z.endswith(".xml")]
         if len(xml_files) != 1:
             raise ValueError("Zip file must contain exactly one XML file!")
-        with zipf.open(xml_files[0]) as f_in, \
-            gzip.open(basename + ".xml.gz", "wb") as f_out:
+        with zipf.open(xml_files[0]) as f_in,\
+                gzip.open(basename + ".xml.gz", "wb") as f_out:
                 shutil.copyfileobj(f_in, f_out)
     os.remove(basename + ".zip")
+
 
 def setup_solver(cobra_model, vmax=1.0e16, solver="cglpk", **optimize_kwargs):
     """Setup an LP solver instance for pFBA that can be recycled.
@@ -89,19 +92,21 @@ def setup_solver(cobra_model, vmax=1.0e16, solver="cglpk", **optimize_kwargs):
     for i, r in enumerate(cobra_model.reactions):
         solver.change_variable_objective(lp, i, 1.0)
         solver.change_variable_bounds(lp, i, 0.0, vmax)
-        if r.objective_coefficient != 0: obj_ids.append(i)
+        if r.objective_coefficient != 0:
+            obj_ids.append(i)
 
     if len(obj_ids) != 1:
-       raise ValueError("Need exactly one objective reaction!")
+        raise ValueError("Need exactly one objective reaction!")
 
     return solver, lp, obj_ids[0]
+
 
 def minimize_total_flux(solver, lp, obj_id, vbm, model, **solver_args):
     """Calculate minimum total flux.
 
-    Obtains the minimum total flux min(sum_i |v_i|) for a given objective value.
-    This function allows the solver and LP to be recycled in order to speed
-    up consecutive runs.
+    Obtains the minimum total flux min(sum_i |v_i|) for a given objective
+    value. This function allows the solver and LP to be recycled in order
+    to speed up consecutive runs.
 
     Args:
         solver: The solver instance to recycle.
@@ -143,7 +148,7 @@ if __name__ == "__main__":
     pred = pred[pred["panel"].isin(tissues["panel"]) & pred["tumor"]]
 
     # Download cancer models from HMA
-    tissues[["url","tissue"]].drop_duplicates().apply(
+    tissues[["url", "tissue"]].drop_duplicates().apply(
         lambda x: download_model(*x), axis=1)
 
     # Calculate pFBA fluxes
@@ -165,17 +170,21 @@ if __name__ == "__main__":
         model.reactions.get_by_id("CancerBiomass_OF").objective_coefficient = 1
         solver, lp, oid = setup_solver(model)
         panels = tissues.loc[ti, "panel"]
-        if type(panels) == str: panels = [panels]
-        else: panels = panels.tolist()
+        if type(panels) == str:
+            panels = [panels]
+        else:
+            panels = panels.tolist()
         data = pred[pred["panel"].isin(panels)]
 
         for i in range(data.shape[0]):
             if co % (n//100) == 0:
                 print("                             \r", end="")
-                print("Calculated {}% of fluxes...".format(ceil(100*co/n)), end="")
+                print("Calculated {}% of fluxes...".format(ceil(100*co/n)),
+                      end="")
             co += 1
             rate = data["rates"].iloc[i]
-            if rate <= 0: continue
+            if rate <= 0:
+                continue
             f = minimize_total_flux(solver, lp, oid, rate, model)
             samples.append(data["patient_barcode"].iloc[i])
             fluxes.append(f)
@@ -192,7 +201,8 @@ if __name__ == "__main__":
     download_model(HMRURL, "hmr")
     model = read_sbml_model(os.path.join("models", "hmr.xml.gz"))
     rids = fluxes.columns.values
-    subsystem = {"id": list(subsystem.keys()), "subsystem": list(subsystem.values())}
+    subsystem = {"id": list(subsystem.keys()),
+                 "subsystem": list(subsystem.values())}
     info = pd.DataFrame(subsystem).set_index("id")
     info = info.loc[fluxes.columns]
 
